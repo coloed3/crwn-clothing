a0a7fc7f402c651aad6b34421c8c21bdaee01a71
Eddie Colon <coloed3@gmail.com>
2019-07-25 06:42:24 -0600

    Finished ShopPage with a collections compoment the Compoment will server as they styles for the title and name of componente

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom'
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
 
-const HatsPage = ()=>{
-  return <div> <h1> hats </h1> </div>
-};
+
 
 
 function App() {
@@ -13,7 +12,7 @@ function App() {
     <div>
       <Switch>
           <Route exact  path='/' component={HomePage} />
-          <Route path='/hats' component={HatsPage}/>
+          <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
