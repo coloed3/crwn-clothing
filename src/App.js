ae4f5647294a06dfcf6600768cbcd359226d779d
Eddie Colon <coloed3@gmail.com>
2019-08-04 15:53:29 -0600

    sign up forms done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
-import {Switch, Route} from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component.jsx";
 import "./App.css";
+import SignInAndSignUpPage from "./pages/sign-in-signup/sign-in-signup.component";
 
 function App() {
-  return (<div>
-    <Header/>
-    <Switch>
-      <Route exact="exact" path="/" component={HomePage}/>
-      <Route path="/shop" component={ShopPage}/>
-    </Switch>
-  </div>);
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
