d72526d2b9a90adea8177a1c3161ca15d60aa1a0
Eddie Colon <coloed3@gmail.com>
2019-08-03 22:35:44 -0600

    working on header

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -7,27 +7,27 @@ import SHOP_DATA from './shop.data.js'
 export class ShopPage extends React.Component {
 
 
-  constructor(props) {
-    super(props);
-    this.state={
-      collections: SHOP_DATA
+    constructor(props) {
+        super(props);
+        this.state={
+            collections: SHOP_DATA
+        }
     }
-  }
 
-  render() {
-    const {collections} =  this.state;
-    return (
-      <div className="shop-page">
-          {
-            collections.map(({id, ...otherCollectionsProps}) =>(
-                  <CollectionPreview key={id} {...otherCollectionsProps}/>
-              ))
-          }
+    render() {
+        const {collections} =  this.state;
+        return (
+            <div className="shop-page">
+                {
+                    collections.map(({id, ...otherCollectionsProps}) =>(
+                        <CollectionPreview key={id} {...otherCollectionsProps}/>
+                    ))
+                }
 
 
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default ShopPage;
